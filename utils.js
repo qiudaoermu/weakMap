236206ec10a64e89b954c5a5b750b5294b5bbe7f
qiudaoermu <1540274138@qq.com>
2018-04-09 18:03:54 +0800

    代码整理

--- utils.js
+++ utils.js
@@ -1,3 +1,19 @@
-export default class a{
+
+class BB {
+    constructor(){}
+    run(){
+        console.log("run")
+    }
+}
+
+
+
+export default class AA extends BB{
+    constructor(){
+            super()
+    }
+    do(){
+        console.log("do it")
+    }
 	
 }
