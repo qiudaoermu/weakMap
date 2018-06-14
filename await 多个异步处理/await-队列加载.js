ccb622d3e9776cfe5900a47766b8af73709361c2
star <1540274138@qq.com>
2018-06-14 10:09:39 +0800

    2018/6/14

--- await 多个异步处理/await-队列加载.js
+++ await 多个异步处理/await-队列加载.js
@@ -1,25 +1,29 @@
-var  fs  = require('fs');
+console.time('a')
+var fs = require('fs');
 var path = require('path');
-var  request = require('request')
+var request = require('request')
 
 var movieDir = __dirname + '/movies',
-    exts     = ['.mkv', '.avi', '.mp4', '.rm', '.rmvb', '.wmv'];
+    exts = ['.mkv', '.avi', '.mp4', '.rm', '.rmvb', '.wmv'];
 
 // 读取文件列表
-var readFiles = function () {
-    return new Promise(function (resolve, reject) {
-        fs.readdir(movieDir, function (err, files) {
+var readFiles = function() {
+    return new Promise(function(resolve, reject) {
+        fs.readdir(movieDir, function(err, files) {
             resolve(files.filter((v) => exts.includes(path.parse(v).ext)));
         });
     });
 };
 
 // 获取海报
-var getPoster = function (movieName) {
+var getPoster = function(movieName) {
     let url = `https://api.douban.com/v2/movie/search?q=${encodeURI(movieName)}`;
 
-    return new Promise(function (resolve, reject) {
-        request({url: url, json: true}, function (error, response, body) {
+    return new Promise(function(resolve, reject) {
+        request({
+            url: url,
+            json: true
+        }, function(error, response, body) {
             if (error) return reject(error);
             console.log(body.subjects[0].images.large)
 
@@ -30,27 +34,19 @@ var getPoster = function (movieName) {
 };
 
 // 保存海报
-var savePoster = function (movieName, url) {
+var savePoster = function(movieName, url) {
     request.get(url).pipe(fs.createWriteStream(path.join(movieDir, movieName + '.jpg')));
 };
 
 
 (async () => {
-    let files = ['中南海保镖','新少林五祖','精武英雄','黑侠','给爸爸的一封信'];
+    let files = ['中南海保镖', '新少林五祖', '精武英雄', '黑侠', '给爸爸的信','新龙门客栈','碧血剑'];
     // await只能使用在原生语法
     for (var file of files) {
-        
         let name = path.parse(file).name;
         console.log(`正在获取【${name}】的海报`);
         savePoster(name, await getPoster(name));
-
     }
     console.log('=== 获取海报完成 ===');
+    console.timeEnd('a')
 })();
-
-
-
-
-
-
-
---
+++ await 多个异步处理/promiseAll同时异步加载.js
@@ -0,0 +1,61 @@
+console.time('a')
+var fs = require('fs');
+var path = require('path');
+var request = require('request')
+
+var movieDir = __dirname + '/movies',
+    exts = ['.mkv', '.avi', '.mp4', '.rm', '.rmvb', '.wmv'];
+
+// 读取文件列表
+var readFiles = function() {
+    return new Promise(function(resolve, reject) {
+        fs.readdir(movieDir, function(err, files) {
+            resolve(files.filter((v) => exts.includes(path.parse(v).ext)));
+        });
+    });
+};
+
+
+
+function Until(){
+  // 保存海报
+  this.savePoster = function (movieName, url) {
+    request.get(url).pipe(fs.createWriteStream(path.join(movieDir, movieName + '.jpg')));
+  }
+  // 获取海报
+  this.getPoster = function(movieName){
+    let url = `https://api.douban.com/v2/movie/search?q=${encodeURI(movieName)}`;
+
+    return new Promise(function(resolve, reject) {
+        request({
+            url: url,
+            json: true
+        }, function(error, response, body) {
+            if (error) return reject(error);
+            console.log(body.subjects[0].images.large)
+
+            resolve(body.subjects[0].images.large);
+            reject(error)
+        })
+    });
+  }
+}
+
+let until = new Until()
+
+let files = ['中南海保镖', '新少林五祖', '精武英雄', '黑侠', '给爸爸的信','新龙门客栈','碧血剑'];
+// await只能使用在原生语法
+let arr = []
+for (var file of files) {
+    let name = path.parse(file).name;
+    console.log(`正在获取【${name}】的海报`);
+    arr.push(until.getPoster(name))
+}
+Promise.all(arr).then((result)=>{
+    console.log(result)
+    result.map((value,index)=>{
+          until.savePoster(files[index],value)
+    })
+    console.timeEnd('a')
+})
+console.log('=== 获取海报完成 ===');
