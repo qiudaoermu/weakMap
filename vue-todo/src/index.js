fc49d3de2cca04a79fbae6d331bb00debc771afe
star <star-chen@stardeMacBook-Pro.local>
2018-08-25 21:24:35 +0800

    2018.8.25

--- vue-todo/src/index.js
+++ vue-todo/src/index.js
@@ -5,18 +5,22 @@ const root = document.createElement('div');
 import VueRouter from 'vue-router'
 import 'mint-ui/lib/style.css'
 import MintUI from 'mint-ui'
+
 Vue.use(MintUI)
 Vue.use(VueRouter)
 
 
 import Index from './app.vue'
 import Address from './page/AddressPage.vue'
-
+import Number from './page/numberGrow.vue'
+import scrollUpAndDown from './page/css3AniamtionScrollTextUpAndDown.vue'
 const router = new VueRouter({
   routes: [
     // 动态路径参数 以冒号开头
     { path: '/', component: Index },
-    { path:'/address',component:Address}
+    { path:'/address',component:Address},
+	  {path:'/bounceNumber',component:Number},
+	  {path:'/scrollUpAndDown',component:scrollUpAndDown},
   ]
 })
 
