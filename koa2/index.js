ad59604802aa161d58ff6163bb5fc6d0b93d8ec5
star <1540274138@qq.com>
2019-07-05 16:35:54 +0800

    2019.7.5

--- koa2/index.js
+++ koa2/index.js
@@ -1,9 +1,41 @@
 const Koa = require( 'koa' )
 const app = new Koa()
 
-app.use( async ( ctx ) => {
-	ctx.body = 'hello koa2'
-} )
+// logger
 
-app.listen( 3000 )
-console.log( '[demo] start-quick is starting at port 3000' )
+app.use(async (ctx, next) => {
+  await next();
+  const rt = ctx.response.get('X-Response-Time');
+  console.log(`${ctx.method} ${ctx.url} - ${rt}`);
+});
+
+// x-response-time
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.set('X-Response-Time', `${ms}ms`);
+});
+
+// response
+
+app.use(async ctx => {
+	if(ctx.request.path= '/test/') {
+		ctx.body = {
+			text: "ok"
+		}
+	} else {
+		ctx.body = 'Hello World';
+	}
+	console.log(ctx.ip)
+	console.log(ctx.query)
+	console.log(ctx.query.name)
+	console.log(ctx.request.path)
+});
+// app.use(function * () {
+// 	console.log(this)
+// 	console.log(ctx.request)
+//   ctx.body = 'Hello World';
+// });
+app.listen(3000);
--- yd-web/README.md
+++ yd-web/README.md
@@ -2,4 +2,4 @@
 // supervisor原理重启
 // 使用cross-env解决跨平台设置NODE_ENV的问题
 // 容错
-// 
+// autowrite
