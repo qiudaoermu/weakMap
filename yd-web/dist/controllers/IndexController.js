44699d071a2c5e1c452e6317d2154bb67f049ede
star <1540274138@qq.com>
2019-07-09 00:54:21 +0800

    2019.7.9

--- yd-web/dist/controllers/IndexController.js
+++ yd-web/dist/controllers/IndexController.js
@@ -1,7 +1,7 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-    value: true
+  value: true
 });
 
 var _IndexModel = require('../models/IndexModel.js');
@@ -11,16 +11,15 @@ var _IndexModel2 = _interopRequireDefault(_IndexModel);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 class IndexController {
-    constructor() {}
-    indexAction() {
-        return async (ctx, next) => {
-            //ctx.body = "<h1>1111</h1>";
-            const IndexModelIns = new _IndexModel2.default();
-            const result = await IndexModelIns.getData();
-            ctx.body = await ctx.render('index', {
-                data: result
-            });
-        };
-    }
+  constructor() {}
+  indexAction() {
+    return async ctx => {
+      const IndexModelIns = new _IndexModel2.default();
+      const result = await IndexModelIns.getData();
+      ctx.body = await ctx.render('index', {
+        data: result
+      });
+    };
+  }
 }
 exports.default = IndexController;
--- yd-web/dist/controllers/index.js
+++ yd-web/dist/controllers/index.js
@@ -11,7 +11,8 @@ var _IndexController2 = _interopRequireDefault(_IndexController);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 const insIndexController = new _IndexController2.default();
-//路由集散中心
+
+// 路由集散中心
 
 exports.default = (app, router) => {
     app.use(router(_ => {
