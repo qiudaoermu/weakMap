e7344c9d9a983a78b15b81a8dc2c5f54c09fba34
chenzhenfei <793385821@qq.com>
2021-09-08 10:57:56 +0800

    [update] 2021/9/08

---
+++ zip/express/README.md
@@ -0,0 +1,12 @@
+## 文件上传服务
+
+启动 pm2 start app.js--watch or node app.js
+
+停止 pm2 stop app.js
+
+上传请命名为 xfj.apk
+
+## 测试火车
+
+1. 文件能否正确上传 大小, 命名
+2. 文件正确能否下载, 覆盖
--- zip/express/until.js
+++ zip/express/until.js
@@ -1,11 +1,11 @@
 var fs = require('fs'); // 引入fs模块
 function deleteall(path) {
   var files = [];
-  if(fs.existsSync(path)) {
+  if (fs.existsSync(path)) {
     files = fs.readdirSync(path);
     files.forEach(function(file, index) {
       var curPath = path + "/" + file;
-      if(fs.statSync(curPath).isDirectory()) { // recurse
+      if (fs.statSync(curPath).isDirectory()) { // recurse
         deleteall(curPath);
       } else { // delete file
         fs.unlinkSync(curPath);
