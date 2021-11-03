6b84aa099455cee6b29bb921698506a206a958fb
chenzhenfei <793385821@qq.com>
2021-11-03 15:55:44 +0800

    ✨

---
+++ 编译原理/jetpp/bundle.js
@@ -0,0 +1,20 @@
+let path = "@/assets/images/";
+let requireModuleKey = require.context("@/assets/images/", false, /\.png$/);
+let imagesNameMap = {};
+for (let i = 0; i < requireModuleKey.keys().length; i) {
+  const moduleName = requireModuleKey.keys()[i].substr(2, requireModuleKey.keys()[i].length);
+  const source = require("@/assets/images/" + moduleName);
+  imagesNameMap[moduleName.replace(/\.png/, "")] = source;
+}
+let requireModule = require.context("@/assets/images/sponsers", false, /\.png$/);
+let imagesNameArr = [];
+for (let i = 0; i < requireModule.keys().length; i) {
+  const moduleName = requireModule.keys()[i].substr(2, requireModule.keys()[i].length);
+  const source = require("@/assets/images/sponsers/" + moduleName);
+  imagesNameArr.push(source);
+}
+var _default = {
+  ...imagesNameMap,
+  sponser: imagesNameArr
+};
+export { _default as default };
---
+++ 编译原理/jetpp/read.md
@@ -0,0 +1,8 @@
+使用 c 语言开发 js 编译器
+
+npm install -g jetpp
+
+
+jetpp main.js --out bundle.js
+
+> https://github.com/vincentdchan/jetpack.js
